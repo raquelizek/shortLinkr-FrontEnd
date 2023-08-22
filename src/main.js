97b6370573d610da8d77c288ecdd511da64b3a5e
raquelizek <raquelreclizek@gmail.com>
2023-08-22 19:51:32 -0300

    feat(main): initialize Vue app and set up routing

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import './assets/css/app.css'
+import router from './router'
+
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
